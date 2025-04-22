26d2a36ae21b2efeba3be0c6c752a5286d30278b
Maxime Moisant <maxime@maximemoisant.fr>
2025-04-22 11:49:01 +0200

    Update bot.rb

--- bot.rb
+++ bot.rb
@@ -4,7 +4,29 @@ require 'dotenv/load'
 
 Dotenv.load
 
+redis_sub = Redis.new(host: 'redis')
+
 bot = Discordrb::Bot.new token: ENV['BOT_TOKEN']
+
+Thread.new do
+  redis_sub.subscribe('discord_messages') do |on|
+    on.message do |channel, message|
+      begin
+        channel_id, *message_parts = message.split(' ', 2)
+        content = message_parts.join(' ')
+        
+        discord_channel = bot.channel(channel_id.to_i)
+        if discord_channel
+          discord_channel.send(content)
+        else
+          puts "Channel #{channel_id} introuvable"
+        end
+      rescue => e
+        puts "Une erreur s'est produite: #{e.message}"
+      end
+    end
+  end
+end
 
 bot.ready do |event|
   puts "Connecté en tant que #{bot.profile.username} (ID: #{bot.profile.id})"
